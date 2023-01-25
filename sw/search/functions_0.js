47196631ed8a0746849f0242e96d5f567cda9551
GHA <push@gha>
2023-01-25 19:19:25 +0000

    update 809fa4b90a415d6dc9e9e4fd9112ec01dc9a8ba0

---
+++ sw/search/functions_0.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['_5f_5fneorv32_5fcpu_5fpmp_5fcfg_5fread_0',['__neorv32_cpu_pmp_cfg_read',['../neorv32__cpu_8c.html#ac0633f560da07ba2df1e693058f5020c',1,'neorv32_cpu.c']]],
+  ['_5f_5fneorv32_5fcpu_5fpmp_5fcfg_5fwrite_1',['__neorv32_cpu_pmp_cfg_write',['../neorv32__cpu_8c.html#a267d5f2b7453c2df0c7e7effe128fd7d',1,'neorv32_cpu.c']]],
+  ['_5f_5fneorv32_5fcrt0_5fafter_5fmain_2',['__neorv32_crt0_after_main',['../neorv32__cpu_8h.html#a0239666ab899179f767f424e0ae1c097',1,'__neorv32_crt0_after_main(int32_t return_code):&#160;main.c'],['../demo__newlib_2main_8c.html#a0239666ab899179f767f424e0ae1c097',1,'__neorv32_crt0_after_main(int32_t return_code):&#160;main.c'],['../processor__check_2main_8c.html#a0239666ab899179f767f424e0ae1c097',1,'__neorv32_crt0_after_main(int32_t return_code):&#160;main.c']]],
+  ['_5f_5fneorv32_5frte_5fcore_3',['__neorv32_rte_core',['../neorv32__rte_8c.html#a53f1e006f0bf61deb7ad018f35228873',1,'neorv32_rte.c']]],
+  ['_5f_5fneorv32_5frte_5fdebug_5fhandler_4',['__neorv32_rte_debug_handler',['../neorv32__rte_8c.html#acd00ae3e9c5b4a0073f7a1a9f4d2079f',1,'neorv32_rte.c']]],
+  ['_5f_5fneorv32_5frte_5fprint_5fcheckbox_5',['__neorv32_rte_print_checkbox',['../neorv32__rte_8c.html#a9e3c7706ae081f79d7e8a3f59a485190',1,'neorv32_rte.c']]],
+  ['_5f_5fneorv32_5frte_5fprint_5fhex_5fhalf_6',['__neorv32_rte_print_hex_half',['../neorv32__rte_8c.html#a784fe17efafc7651c844d12f6801ab3f',1,'neorv32_rte.c']]],
+  ['_5f_5fneorv32_5frte_5fprint_5fhex_5fword_7',['__neorv32_rte_print_hex_word',['../neorv32__rte_8c.html#a01bf6c01f5a8cda2132b9ac98824edc1',1,'neorv32_rte.c']]],
+  ['_5f_5fneorv32_5frte_5fprint_5ftrue_5ffalse_8',['__neorv32_rte_print_true_false',['../neorv32__rte_8c.html#aa12bb3086da1be887a0d4c9505e3f710',1,'neorv32_rte.c']]],
+  ['_5f_5fneorv32_5fuart_5fitoa_9',['__neorv32_uart_itoa',['../neorv32__uart_8c.html#a3543435827204d82755b0a23bdea1296',1,'neorv32_uart.c']]],
+  ['_5f_5fneorv32_5fuart_5ftohex_10',['__neorv32_uart_tohex',['../neorv32__uart_8c.html#aefa844cd170e50e088c42829029060f3',1,'neorv32_uart.c']]],
+  ['_5f_5fneorv32_5fuart_5ftouppercase_11',['__neorv32_uart_touppercase',['../neorv32__uart_8c.html#adf807d0476b475276b8b29924297cda9',1,'neorv32_uart.c']]],
+  ['_5f_5fneorv32_5fxirq_5fcore_12',['__neorv32_xirq_core',['../neorv32__xirq_8c.html#a14661d628dd31e6bfce89376c6614627',1,'neorv32_xirq.c']]],
+  ['_5f_5fneorv32_5fxirq_5fdummy_5fhandler_13',['__neorv32_xirq_dummy_handler',['../neorv32__xirq_8c.html#ae4d9362b23a1341c595a4cd239a87091',1,'neorv32_xirq.c']]]
+];
